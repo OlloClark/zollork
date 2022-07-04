1d3745839e13b50587abbae6c7741f7a4266fe34
OlloClark <ollo42@hotmail.co.uk>
2022-07-04 19:45:52 +0100

    taking name out of while loop

--- zollork.py
+++ zollork.py
@@ -3,16 +3,17 @@ choice = 0
 name = input(f"who are you? \n {prompt}")
 
 if name:
+	print(f"Hello, {name}")
 	choice = 1
 
 while choice == 1:
 	if choice == 1:
-		table_choice = input(f"Hello, {name}. Look at the table \n {prompt}")
+		table_choice = input(f"Look at the table \n {prompt}")
 
 	if table_choice.lower() == "look at table":
 		choice = 2
 	elif table_choice.lower() == "what?":
-		print("What do you mean 'what'? I said look at the table")
+		print("What do you mean 'what'? I said look at the table.")
 	elif table_choice.lower() == "I refuse":
 		print("you cannot refuse")
 	else:
@@ -21,4 +22,4 @@ while choice == 1:
 while choice == 2:
 	if choice == 2:
 		weapon_choice = input(f"time to choose a weapon! \n {prompt}")
-		print(f"you chose {weapon_choice}. Nice")
+		print(f"you chose {weapon_choice.lower()}. Nice")
