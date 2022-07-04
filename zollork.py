1e80c9bd6c4241e22afbdbc7e8415023752aeffd
OlloClark <ollo42@hotmail.co.uk>
2022-07-04 19:40:22 +0100

    establishing while loops

--- zollork.py
+++ zollork.py
@@ -1,18 +1,24 @@
 prompt = "..."
+choice = 0
+name = input(f"who are you? \n {prompt}")
 
-name = input(f"""
-who are you?
-{prompt}
-""")
+if name:
+	choice = 1
 
-table_choice = input(f"""
-Hello, {name}. Look at the table
-{prompt}
-""")
+while choice == 1:
+	if choice == 1:
+		table_choice = input(f"Hello, {name}. Look at the table \n {prompt}")
 
-if table_choice.lower() === "what":
-	input("I said look at the table")
-elif table_choice.lower() === "look at table":
-	print("there are three weapons")
-else:
-	input("no decision made")
+	if table_choice.lower() == "look at table":
+		choice = 2
+	elif table_choice.lower() == "what?":
+		print("What do you mean 'what'? I said look at the table")
+	elif table_choice.lower() == "I refuse":
+		print("you cannot refuse")
+	else:
+		print("..............")
+
+while choice == 2:
+	if choice == 2:
+		weapon_choice = input(f"time to choose a weapon! \n {prompt}")
+		print(f"you chose {weapon_choice}. Nice")
